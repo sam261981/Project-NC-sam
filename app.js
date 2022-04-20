5ea55d40ab605c6e6637c3a5b436f85a1aaa963f
sam261981 <sam_zo@hotmail.co.uk>
2022-04-20 17:47:21 +0100

    install cros

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
+const cors = require('cors')
 const express = require('express')
 const { getTopic } = require('./controllers/topics.controller')
-const cors = require('cors')
 const app = express()
 app.use(cors())
 
