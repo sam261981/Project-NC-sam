eae3ba5f7e68203d8f81e8b71856e8d06ab70169
sam261981 <sam_zo@hotmail.co.uk>
2022-04-20 11:23:49 +0100

    done

--- listen.js
+++ listen.js
@@ -1,3 +1,8 @@
 const app = require('./app')
 const { PORT = 9090 } = process.env
-app.listen(PORT)
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.log('server listen on the port', PORT)
+})
